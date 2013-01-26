e4e33f8ff9d16b66c9794c5548ada9f3f82daf54
Tomasz Mloduchowski <q@qdot.me>
2013-01-27 00:49:51 +0100

    version supporting 'Reason for absence of number'

--- QMDMF/qmdmf.cpp
+++ QMDMF/qmdmf.cpp
@@ -25,11 +25,11 @@ void QMDMF::onDataAvailable()
 
 void QMDMF::parseMDMF(QByteArray data) {
     if (data.at(0) != (char)0x80) {
-        qDebug() << __PRETTY_FUNCTION__ << data.toPercentEncoding() << "Not CallerID data[0]";
+        qDebug() << __FILE__ << __LINE__ << __PRETTY_FUNCTION__ << data.toPercentEncoding() << "Not CallerID data[0]";
         return;
     }
     if (data.at(1) != data.length() - 3) {
-        qDebug() << __PRETTY_FUNCTION__ << data.toPercentEncoding() << "Length error - expected" << (int)data.at(1) << "got" << data.length();
+        qDebug() << __FILE__ << __LINE__ << __PRETTY_FUNCTION__ << data.toPercentEncoding() << "Length error - expected" << (int)data.at(1) << "got" << data.length();
         return;
     }
     /// @todo: Checksum
@@ -37,22 +37,34 @@ void QMDMF::parseMDMF(QByteArray data) {
     for (int i=2; i+1 < data.length(); ) {
         switch (data.at(i)) {
         case 0x11:
-            qDebug() << "Call Type Message";
+            qDebug() << __FILE__ << __LINE__ << __PRETTY_FUNCTION__ << "Call Type Message";
             break;
         case 0x01:
-            qDebug() << "Date & Time Parameter";
+            qDebug() << __FILE__ << __LINE__ << __PRETTY_FUNCTION__ << "Date & Time Parameter";
             break;
         case 0x02:
-            qDebug() << "Calling Number Parameter";
+            qDebug() << __FILE__ << __LINE__ << __PRETTY_FUNCTION__ << "Calling Number Parameter";
             n = data.mid(i+2, data.at(i+1));
-            qDebug() << "Extracted Number" << n;
+            qDebug() << __FILE__ << __LINE__ << __PRETTY_FUNCTION__ << "Extracted Number" << n;
             emit VCIDNMBR(n);
             break;
+        case 0x04:
+            qDebug() << __FILE__ << __LINE__ << __PRETTY_FUNCTION__ << "Reason for absence of number";
+            if (data.at(i+2) == 'P') {
+                emit VCIDNMBR("WITHHELD");
+                break;
+            }
+            if (data.at(i+2) == 'O') {
+                emit VCIDNMBR("UNAVAILABLE");
+                break;
+            }
+            qDebug() << __FILE__ << __LINE__ << __PRETTY_FUNCTION__ << "Cannot understand reasons: " << data.mid(i+2, 1).toHex();
+            break;
         case 0x07:
-            qDebug() << "Calling Name Parameter";
+            qDebug() << __FILE__ << __LINE__ << __PRETTY_FUNCTION__ << "Calling Name Parameter";
             break;
         default:
-            qDebug() << "Cannot understand Parameter: 0x" << data.mid(i, 1).toHex();
+            qDebug() << __FILE__ << __LINE__ << __PRETTY_FUNCTION__ << "Cannot understand Parameter: 0x" << data.mid(i, 1).toHex();
         }
         i += data.at(i+1) + 2;
     }
